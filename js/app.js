4678740fc49745fb912bfd5aff45a6686c10f3b3
Varun Naik <varun.naik@gmail.com>
2014-08-23 15:36:06 +0530

    Integrate Markdown helpers.

--- js/app.js
+++ js/app.js
@@ -36,6 +36,12 @@ Ember.Handlebars.helper('format-date', function(date) {
 	return moment(date).fromNow();
 });
 
+var showdown = new Showdown.converter();
+
+Ember.Handlebars.helper('format-markdown', function(input) {
+	return new Handlebars.SafeString(showdown.makeHtml(input));
+});
+
 var posts = [
 	{
 		id: '1',
